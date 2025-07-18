036ec7118b9f6c9be2780c7332e009378bb88c9e
rolindaz <rolinda.zamboni@gmail.com>
2025-07-18 14:05:14 +0200

    B1: Add trash icon by generating it with the list item within the map cycle

--- bonus/src/App.jsx
+++ bonus/src/App.jsx
@@ -1,10 +1,10 @@
 /* 
 Consegna:
-Milestone 1
-Creare una pagina che visualizzi una lista di articoli, mostrandone solo il titolo.
+Bonus 1
+Aggiungere la possibilità di cancellare ciascun articolo utilizzando un'icona.
 */
 
-// Creo una lista di articoli in data/posts e la esporto e importo in app.jsx (potrò in un secondo momento creare un componente ad hoc per la visualizzazione degli articoli)
+// Devo aggiungere in pagina l'icona generandola insieme al list item nel ciclo map
 // Per visualizzare la lista creo una ul stilizzata e ciclo dentro l'array di articoli usando il metodo map() e isolo i titoli con la dot notation selezionando la proprietà title dell'oggetto post
 
 /*
@@ -27,18 +27,7 @@ import postsList from '../data/postsList'
 function App() {
 
   const [newPost, setNewPost] = useState('')
-  /* const titles = []
-  {
-    postsList.forEach((post) => {
-      titles.push(post.title)
-      return titles
-    })
-    console.log(titles);
-  } */
-  {/* M2: imposto una nuova associazione con lo stato questa volta direttamente alla lista di post, stabilendo come stato di partenza l'array iniziale di articoli */ }
   const [posts, setPosts] = useState(postsList)
-
-  {/* M2: creo una funzione submitHandler che al verificarsi dell'evento submit clona l'array di partenza (tramite l'operatore spread) e richiama il setState per aggiornarla con il nuovo elemento inserito tramite form. Siccome dobbiamo utilizzare il dato inserito impediamo il refresh della pagina con il preventDefault */ }
 
   function submitHandler(e) {
     e.preventDefault()
@@ -49,25 +38,26 @@ function App() {
   return (
     <>
       <div className="container p-3">
-        {/* M1: Creo una ul stilizzata */}
         <ul className="list-group">
-          {/* M1: ciclo dentro l'array di post usando il metodo map() */}
+          {/* B1: Aggiungo in pagina l'icona generandola insieme al list item nel ciclo map */}
           {
             posts.map((post, index) => {
               return (
-                <li key={index} className="list-group-item">
-                  {/* M1: isolo i titoli con la dot notation selezionando la proprietà title dell'oggetto post */}
-                  {post.title}
-                </li>
+                <>
+                  <li key={index} className="list-group-item d-flex justify-content-between">
+                    <span>
+                      {post.title}
+                    </span>
+                    <button className='border-0 bg-transparent'>
+                      <i className="bi bi-trash3-fill"></i>
+                    </button>
+                  </li>
+                </>
               )
             })
           }
         </ul>
-        {/* M2: Creo l'elemento form con l'input e il button per il submit */}
-        {/* M2: Applichiamo il submitHandler all'evento onSubmit del form */}
         <form className='mt-3' onSubmit={submitHandler}>
-          {/* M2: Collego l'inserimento di un nuovo post allo stato del componente tramite l'attributo value di input */}
-          {/* M2: Uso l'evento onChange per creare un legame bilaterale tra l'input e lo stato */}
           <input
             className='form-control my-2'
             placeholder='Insert new post title'
