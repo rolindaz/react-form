1b7be6ec93f5ae83d7cb772e4abbf438b37fd430
rolindaz <rolinda.zamboni@gmail.com>
2025-07-18 14:40:40 +0200

    B2: Add edit icon

--- bonus/src/App.jsx
+++ bonus/src/App.jsx
@@ -6,20 +6,16 @@ Aggiungere la possibilità di cancellare ciascun articolo utilizzando un'icona.
 
 // Devo aggiungere in pagina l'icona generandola insieme al list item nel ciclo map
 // Creo una funzione postRemover che elimina un post in base all'indice, genera un nuovo array in cui quell'indice non è incluso grazie a filter e richiama il setState per aggiornare la lista
-// Richiamo il postRemover all'evento onClick del bottone contenente l'icona
+// Richiamo il postRemover nell'evento onClick del bottone contenente l'icona
 
 /*
 Consegna:
-Milestone 2
-Aggiungiamo in pagina un semplice form con un campo input in cui inserire il titolo di un nuovo articolo del blog. Al submit del form, mostrare la lista degli articoli aggiornati.
+Bonus 2
+Implementare la funzionalità di modifica del titolo di un post.
 */
 
-// Creo l'elemento form con l'input e il button per il submit
-// Collego l'inserimento di un nuovo post allo stato del componente tramite l'attributo value di input
-// Uso l'evento onChange per creare un legame bilaterale tra l'input e lo stato - a questo punto React associa un nuovo stato con ciò che noi abbiamo inserito nell'input al componente ma per aggiornare la lista abbiamo bisogno di associare un ulteriore cambiamento all'evento submit
-// Quindi imposto una nuova associazione con lo stato questa volta direttamente alla lista di post, stabilendo come stato di partenza l'array iniziale
-// Poi creo una funzione submitHandler che al verificarsi dell'evento submit clona l'array di partenza e richiama il setState per aggiornarla con il nuovo elemento inserito tramite form. Siccome dobbiamo utilizzare il dato inserito impediamo il refresh della pagina con il preventDefault
-// Applichiamo il submitHandler all'evento onSubmit del form
+// Aggiungo in pagina l'icona per la modifica generandola insieme al list item nel ciclo map
+// 
 
 
 import { useState } from 'react'
@@ -48,7 +44,6 @@ function App() {
     <>
       <div className="container p-3">
         <ul className="list-group">
-          {/* B1: Aggiungo in pagina l'icona generandola insieme al list item nel ciclo map */}
           {
             posts.map((post, i) => {
               return (
@@ -57,8 +52,12 @@ function App() {
                     <span>
                       {post.title}
                     </span>
+                    {/* B1: Richiamo il postRemover nell'evento onClick del bottone contenente l'icona */}
                     <button className='border-0 bg-transparent' onClick={() => { postRemover(i) }}>
+                      {/* B1: Aggiungo in pagina l'icona generandola insieme al list item nel ciclo map */}
                       <i className="bi bi-trash3-fill"></i>
+                      {/* B2: Aggiungo in pagina l'icona per la modifica generandola insieme al list item nel ciclo map */}
+                      <i className="bi bi-pencil-fill"></i>
                     </button>
                   </li>
                 </>
